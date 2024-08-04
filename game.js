9b28fad2a7d99cec234e652f9c149a75eb45a1a3
BabyBlueWhale <baby.blue.whale.sol@gmail.com>
2024-08-04 19:10:28 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -12,25 +12,33 @@ let score = 0;
 let timeElapsed = 0;
 let player;
 let obstacles = [];
+let fishItems = [];
 let trashItems = [];
+let powerUps = [];
 let gameInterval;
 let timerInterval;
 let difficultyInterval;
 let isGameOver = false;
-let trashSpeed = 3;
+let fishSpeed = 3;
 let obstacleSpeed = 2;
 
 const images = {
     background: new Image(),
     octopus: new Image(),
+    fish: new Image(),
     trash: new Image(),
-    whale: new Image()
+    trash2: new Image(),
+    whale: new Image(),
+    speedBoost: new Image()
 };
 
 images.background.src = 'blue ocean.webp';
 images.octopus.src = 'octupus.png';
+images.fish.src = 'fish.png';
 images.trash.src = 'trash.png';
+images.trash2.src = 'trash2.png';
 images.whale.src = 'whale.png';
+images.speedBoost.src = 'speed_boost.png';
 
 class Player {
     constructor() {
@@ -106,17 +114,67 @@ class Obstacle {
     }
 }
 
-class Trash {
+class Fish {
     constructor(x, y) {
         this.x = x;
         this.y = y;
         this.width = 40;
         this.height = 40;
-        this.speed = trashSpeed;
+        this.speed = fishSpeed;
     }
 
     draw() {
-        ctx.drawImage(images.trash, this.x, this.y, this.width, this.height);
+        ctx.drawImage(images.fish, this.x, this.y, this.width, this.height);
+    }
+
+    update() {
+        this.y += this.speed;
+        if (this.y > canvas.height) {
+            this.y = -this.height;
+            this.x = Math.random() * (canvas.width - this.width);
+        }
+    }
+}
+
+class Trash {
+    constructor(x, y, type = 'trash') {
+        this.x = x;
+        this.y = y;
+        this.width = 40;
+        this.height = 40;
+        this.speed = fishSpeed;
+        this.type = type;
+    }
+
+    draw() {
+        if (this.type === 'trash') {
+            ctx.drawImage(images.trash, this.x, this.y, this.width, this.height);
+        } else {
+            ctx.drawImage(images.trash2, this.x, this.y, this.width, this.height);
+        }
+    }
+
+    update() {
+        this.y += this.speed;
+        if (this.y > canvas.height) {
+            this.y = -this.height;
+            this.x = Math.random() * (canvas.width - this.width);
+        }
+    }
+}
+
+class PowerUp {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = 40;
+        this.height = 40;
+        this.speed = 2;
+        this.type = 'speedBoost';
+    }
+
+    draw() {
+        ctx.drawImage(images.speedBoost, this.x, this.y, this.width, this.height);
     }
 
     update() {
@@ -132,23 +190,35 @@ function startGame() {
     isGameOver = false;
     score = 0;
     timeElapsed = 0;
-    trashSpeed = 3;
+    fishSpeed = 3;
     obstacleSpeed = 2;
     scoreElement.textContent = `Score: ${score}`;
     timerElement.textContent = `Time: ${timeElapsed}`;
     leaderboardElement.style.display = 'none';
     obstacles = [];
+    fishItems = [];
     trashItems = [];
+    powerUps = [];
     player = new Player();
-    for (let i = 0; i < 5; i++) {
-        obstacles.push(new Obstacle(Math.random() * canvas.width, Math.random() * canvas.height - canvas.height));
-        trashItems.push(new Trash(Math.random() * canvas.width, Math.random() * canvas.height - canvas.height));
+    for (let i = 0; i < 20; i++) { // 4x more fish
+        fishItems.push(new Fish(Math.random() * canvas.width, Math.random() * canvas.height - canvas.height));
     }
+    for (let i = 0; i < 7; i++) { // 30% less obstacles (7 instead of 10)
+        obstacles.push(new Obstacle(Math.random() * canvas.width, Math.random() * canvas.height - canvas.height));
+    }
+    for (let i = 0; i < 10; i++) { // Double trash
+        trashItems.push(new Trash(Math.random() * canvas.width, Math.random() * canvas.height - canvas.height, 'trash'));
+        // Adding trash2 with a 50% chance relative to trash
+        if (Math.random() < 0.33) { // Adjusted for 50% less than trash
+            trashItems.push(new Trash(Math.random() * canvas.width, Math.random() * canvas.height - canvas.height, 'trash2'));
+        }
+    }
+    powerUps.push(new PowerUp(Math.random() * canvas.width, Math.random() * canvas.height - canvas.height));
     gameInterval = setInterval(updateGame, 1000 / 60);
     timerInterval = setInterval(updateTimer, 1000);
     difficultyInterval = setInterval(() => {
         increaseDifficulty(true);
-    }, 15000); // Every 15 seconds
+    }, 15000);
 }
 
 function updateGame() {
@@ -167,18 +237,37 @@ function updateGame() {
         }
     });
 
+    fishItems.forEach(fish => {
+        fish.update();
+        fish.draw();
+    });
+
     trashItems.forEach(trash => {
         trash.update();
         trash.draw();
         if (collision(player, trash)) {
-            score++;
-            scoreElement.textContent = `Score: ${score}`;
+            if (trash.type === 'trash2') {
+                score += 5;
+            } else {
+                score++;
+            }
             trash.y = -trash.height;
             trash.x = Math.random() * canvas.width;
+            scoreElement.textContent = `Score: ${score}`;
 
             if (score % 10 === 0) {
-                increaseDifficulty(false); // Every 10 points
+                increaseDifficulty(false);
             }
+        }
+    });
+
+    powerUps.forEach(powerUp => {
+        powerUp.update();
+        powerUp.draw();
+        if (collision(player, powerUp)) {
+            activatePowerUp(powerUp.type);
+            powerUp.y = -powerUp.height;
+            powerUp.x = Math.random() * canvas.width;
         }
     });
 }
@@ -194,24 +283,38 @@ function increaseDifficulty(fromTimer) {
     if (isGameOver) return;
 
     if (fromTimer || score % 10 === 0) {
-        trashSpeed += 0.5;
+        fishSpeed += 0.5;
         obstacleSpeed += 0.5;
 
         obstacles.forEach(obstacle => {
             obstacle.speed = obstacleSpeed;
         });
 
+        fishItems.forEach(fish => {
+            fish.speed = fishSpeed;
+        });
+
         trashItems.forEach(trash => {
-            trash.speed = trashSpeed;
+            trash.speed = fishSpeed;
         });
     }
 }
 
 function collision(obj1, obj2) {
-    return obj1.x < obj2.x + obj2.width &&
-           obj1.x + obj1.width > obj2.x &&
-           obj1.y < obj2.y + obj2.height &&
-           obj1.y + obj1.height > obj2.y;
+    const overlapRatio = 0.6; // 60% overlap
+    const marginX = obj1.width * (1 - overlapRatio) / 2;
+    const marginY = obj1.height * (1 - overlapRatio) / 2;
+    return obj1.x + marginX < obj2.x + obj2.width &&
+           obj1.x + obj1.width - marginX > obj2.x &&
+           obj1.y + marginY < obj2.y + obj2.height &&
+           obj1.y + obj1.height - marginY > obj2.y;
+}
+
+function activatePowerUp(type) {
+    if (type === 'speedBoost') {
+        player.speed *= 2;
+        setTimeout(() => player.speed /= 2, 5000);
+    }
 }
 
 function endGame() {
